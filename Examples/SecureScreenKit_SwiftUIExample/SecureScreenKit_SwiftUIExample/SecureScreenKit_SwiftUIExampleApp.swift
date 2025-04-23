dd537878ac88540a7ee0482da9a41d29c905f672
Byron Mejia <byroniosdeveloper@gmail.com>
2025-04-22 22:25:08 -0600

    Add examples

---
+++ Examples/SecureScreenKit_SwiftUIExample/SecureScreenKit_SwiftUIExample/SecureScreenKit_SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureScreenKit_SwiftUIExampleApp.swift
+//  SecureScreenKit_SwiftUIExample
+//
+//  Created by Byron on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureScreenKit_SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/SecureScreenKit_UIKitExample/SecureScreenKit_UIKitExample/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  SecureScreenKit_UIKitExample
+//
+//  Created by Byron on 4/22/25.
+//
+
+import UIKit
+import SecureScreenKit
+
+class ViewController: ScreenProtectedViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapButton(_ sender: Any) {
+        NotificationCenter.default.post(
+            name: UIApplication.userDidTakeScreenshotNotification,
+            object: nil
+        )
+
+        presentScreenshotAlert()
+    }
+}
+
+private extension ViewController {
+
+    func presentScreenshotAlert() {
+        let alert = UIAlertController(
+            title: "Screenshot Detected",
+            message: "This alert simulates a screenshot detection.",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default
+            )
+        )
+
+        present(alert, animated: true)
+    }
+}
